3f9ce9812116fac5c33d2fff98985d31abb18ca1
Apala Singh <apalasingh2001@gmail.com>
2021-09-03 23:40:48 +0530

    in progress

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sgpa/>
+    <cgpa/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import cgpa from './components/cgpa.vue'
+import sgpa from './components/sgpa.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    cgpa,
+    sgpa,
   }
 }
 </script>
@@ -23,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/sgpa.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <!-- <i class="fab fa-medium"></i>
+<i class="far fa-envelope"></i> -->
+    <h1
+      class="heading is-size-3 has-text-centered is-underlined has-text-weight-bold is-family-monospace"
+    >
+      SGPA-CGPA CALCULATOR
+    </h1>
+    <div class="box mt-6 tabs is-toggle is-toggle-rounded is-centered">
+      <ul>
+        <li @click="show=false" :class="{'is-active': !show}">
+          <a>
+            <span class="icon is-small"><i class="fas fa-graduation-cap"></i></span>
+            <span>SGPA</span>
+          </a>
+        </li>
+        <li @click="show=true" :class="{'is-active': show}" >
+          <a>
+            <span class="icon is-small"><i class="fas fa-graduation-cap"></i></span>
+            <span>CGPA</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            show: false,
+            
+        }
+    }
+};
+</script>
+
+<style scoped></style>
